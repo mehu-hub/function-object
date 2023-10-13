cf129421a35a4ebbe1936315d39e035831902fec
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-14 01:33:17 +0600

    Parameter Details

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# function-object" 
+<h1>Function & Object</h1>
+<ul>
+  <li>Function</li>
+  <li>Parameter</li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ul>
--- parameter.js
+++ parameter.js
@@ -1,7 +1,25 @@
-function bringSingara(money){
-    console.log('Mama Singara den');
-    console.log(money);
+function bringSingara(money) {
+    // console.log('Give Money:', money);
+    // console.log('Mama Singara den');
+    // console.log(money);
 }
 
+//------------------------------------------
+
+var taka = 300;
 // call function
-bringSingara(250);
+// bringSingara(taka);
+
+function add(num1, num2) {
+    console.log('Going to add :', num1, num2);
+}
+// add(10, 10);
+
+//----------------------------------
+function sum(a, b, c, d, e) {
+    console.log(a, b , c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+
+sum(15, 51, 10, 52, 88);
