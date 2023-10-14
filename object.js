78c82e7adcd6fdfe33b711d1542e509c5ec7dde5
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-14 08:44:49 +0600

    Object

---
+++ object.js
@@ -0,0 +1,21 @@
+var student = {
+    id: 15,
+    name: 'Mehedi Hasan',
+    semester: '8th Semester',
+    marks: '3.25'
+};
+var mobile = {
+    brand: 'Realme',
+    price: 20000,
+    ram: 8,
+    rom: 128,
+    color: 'teal'
+}
+var myComputer = {
+    brand: 'HP',
+    price: 45000,
+    ram: '8gb',
+    ssd: '240gb',
+    processor: 'AMD Ryzen'
+}
+console.log(myComputer.ssd);
